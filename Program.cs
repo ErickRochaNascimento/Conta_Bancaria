02270a403dfe7947b5b73c3c5c16e9734029fda7
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2026-02-05 10:50:20 -0300

    Upload arquivo inicial

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+contaBancaria contaBancaria1 = new contaBancaria();
+contaBancaria1.nomeTitular = "Erick Nascimento";
+contaBancaria1.numeroConta = 2026020001;
+contaBancaria1.saldo = 5000;
+
+contaBancaria contaBancaria2 = new contaBancaria();
+contaBancaria2.nomeTitular = "Matheus Luciano";
+contaBancaria2.numeroConta = 2026020002;
+contaBancaria2.saldo = 2000;
+
+contaBancaria1.ExibirInformacaoConta();
+contaBancaria2.ExibirInformacaoConta();
---
+++ contaBancaria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class contaBancaria
+{
+
+    public string nomeTitular;
+    public int numeroConta;
+    public decimal saldo;
+    
+    public void ExibirInformacaoConta()
+    {
+        Console.WriteLine($"Conta informações: \n Nome titular{nomeTitular} \n Numero conta: {numeroConta} \n Saldo: {saldo}");
+    }
+}
